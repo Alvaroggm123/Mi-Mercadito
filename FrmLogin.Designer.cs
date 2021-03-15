92d1dcd9076416c12bbeff20468049f6f20fce6d
Alvaroggm123 <aggm123@icloud.com>
2021-03-15 16:02:35 -0700

    Se agregó la imagen principal.

--- FrmLogin.Designer.cs
+++ FrmLogin.Designer.cs
@@ -42,6 +42,7 @@ namespace Mi_mercadito
             // 
             // pbxLogo
             // 
+            this.pbxLogo.Image = global::Mi_mercadito.Properties.Resources.Logo_mi_mercadito__03_10_21_4;
             this.pbxLogo.Location = new System.Drawing.Point(23, 12);
             this.pbxLogo.Name = "pbxLogo";
             this.pbxLogo.Size = new System.Drawing.Size(256, 256);
@@ -68,6 +69,7 @@ namespace Mi_mercadito
             // 
             this.txtPassword.Location = new System.Drawing.Point(90, 308);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(189, 20);
             this.txtPassword.TabIndex = 3;
             // 
@@ -113,7 +115,7 @@ namespace Mi_mercadito
             this.AcceptButton = this.cmdAccept;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 450);
+            this.ClientSize = new System.Drawing.Size(304, 422);
             this.Controls.Add(this.lblTerminos);
             this.Controls.Add(this.cmdRegister);
             this.Controls.Add(this.cmdAccept);
