5e52b65208e08a2aba2a2be46074f7931c809566
Alvaroggm123 <aggm123@icloud.com>
2021-03-15 15:11:48 -0700

    Add project files.

---
+++ FrmLogin.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Mi_mercadito
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.cmdAccept = new System.Windows.Forms.Button();
+            this.cmdRegister = new System.Windows.Forms.Button();
+            this.lblTerminos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Location = new System.Drawing.Point(23, 12);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(256, 256);
+            this.pbxLogo.TabIndex = 0;
+            this.pbxLogo.TabStop = false;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(20, 285);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(99, 13);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Nombre de usuario:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(125, 282);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(154, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(90, 308);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(189, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(20, 311);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 13);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Contraseña:";
+            // 
+            // cmdAccept
+            // 
+            this.cmdAccept.Location = new System.Drawing.Point(55, 347);
+            this.cmdAccept.Name = "cmdAccept";
+            this.cmdAccept.Size = new System.Drawing.Size(82, 23);
+            this.cmdAccept.TabIndex = 4;
+            this.cmdAccept.Text = "iniciar sesión ";
+            this.cmdAccept.UseVisualStyleBackColor = true;
+            this.cmdAccept.Click += new System.EventHandler(this.cmdAccept_Click);
+            // 
+            // cmdRegister
+            // 
+            this.cmdRegister.Location = new System.Drawing.Point(157, 347);
+            this.cmdRegister.Name = "cmdRegister";
+            this.cmdRegister.Size = new System.Drawing.Size(80, 23);
+            this.cmdRegister.TabIndex = 5;
+            this.cmdRegister.Text = "Registrarme";
+            this.cmdRegister.UseVisualStyleBackColor = true;
+            // 
+            // lblTerminos
+            // 
+            this.lblTerminos.AutoSize = true;
+            this.lblTerminos.Location = new System.Drawing.Point(87, 392);
+            this.lblTerminos.Name = "lblTerminos";
+            this.lblTerminos.Size = new System.Drawing.Size(118, 13);
+            this.lblTerminos.TabIndex = 6;
+            this.lblTerminos.Text = "Terminos y condiciones";
+            // 
+            // FrmLogin
+            // 
+            this.AcceptButton = this.cmdAccept;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 450);
+            this.Controls.Add(this.lblTerminos);
+            this.Controls.Add(this.cmdRegister);
+            this.Controls.Add(this.cmdAccept);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.pbxLogo);
+            this.Name = "FrmLogin";
+            this.Text = "iniciar sesión ";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button cmdAccept;
+        private System.Windows.Forms.Button cmdRegister;
+        private System.Windows.Forms.Label lblTerminos;
+    }
+}
+
---
+++ FrmLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mi_mercadito
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdAccept_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
